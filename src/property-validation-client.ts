d3ef9d56df841a12a7c6097a7ab0514bc1bc725a
Wil Maier <48260215+wilmaier@users.noreply.github.com>
2022-03-24 10:55:33 -0400

    Various PR comment fixes.

--- src/property-validation-client.ts
+++ src/property-validation-client.ts
@@ -2,4 +2,8 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-export * from "./PropertyValidationClientExports";
+export * from "./base";
+export * from "./operations";
+export * from "./Constants";
+export * from "./PropertyValidationClient";
+
